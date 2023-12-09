5ba197ef8408dc63b53094c3e7321fc0bef1ece0
teppei-kaneshiro <teppei@miraistudio.co.jp>
2023-12-09 15:40:17 +0900

    ログイン画面ドラフト

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Tutorial />
+  <div>
+    <form>
+      <p><input type="text" v-model="email" placeholder="メールアドレス" name="email" /></p>
+      <p><input type="text" v-model="password" placeholder="パスワード" name="password" /></p>
+      <div>
+        <button type="submit">ログイン</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }
 }
 </script>
