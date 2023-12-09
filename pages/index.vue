724c27af2b6a8ac564d022cb33b0e7c85b60dbd5
yuha-yuha <ishizushimadev@gmail.com>
2023-12-09 17:10:41 +0900

    login css書いた

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <form>
-      <p><input type="text" v-model="email" placeholder="メールアドレス" name="email" /></p>
-      <p><input type="text" v-model="password" placeholder="パスワード" name="password" /></p>
+  <div class="login-feild">
+    <form class="login-form">
+      <p><input type="text" v-model="email" placeholder="メールアドレス" name="email" class="user-email" /></p>
+      <p><input type="text" v-model="password" placeholder="パスワード" name="password" class="user-password" /></p>
       <div>
-        <button type="submit">ログイン</button>
+        <button type="submit" class="login-form-btn">ログイン</button>
       </div>
     </form>
   </div>
@@ -21,3 +21,34 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.login-field {
+  margin: 50%;
+}
+.user-email {
+    width: 50%;
+    height: 8vh;
+    margin: auto;
+    text-align: center;
+    color: black;
+}
+.user-password {
+  width: 50%;
+  height: 8vh;
+  margin: auto;
+  text-align: center;
+  color: black;
+}
+
+.login-form {
+  text-align: center;
+  margin-top: 30vh;
+}
+.login-form-btn {
+  width: 30%;
+  height: 6vh;
+  text-align: center;
+}
+
+</style>
