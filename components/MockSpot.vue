16a51bedb49d5ea028527767274157925323e29e
teppei-kaneshiro <teppei@miraistudio.co.jp>
2023-12-09 16:51:07 +0900

    add: スポットリスト作成

---
+++ components/MockSpot.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-row>
+      <v-col>{{ spotData.congestion }}</v-col>
+      <v-col>{{ spotData.name }}</v-col>
+      <v-col>{{ spotData.address }}</v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    spotData: Object
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/spot_list.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="spot in spotList" :key="spot.id">
+      <mock-spot :spot-data="spot" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      spotList: [
+        { id: 1, congestion: '混雑', name: '美ら海水族館', address: '国頭郡本部町石川424' },
+        { id: 2, congestion: '普通', name: 'エイサー会場', address: '諸見里2丁目1-1' },
+        { id: 3, congestion: '空き', name: '琉球ガラス村', address: '糸満市福地169' },
+        { id: 4, congestion: '普通', name: '陶芸JIN', address: '島尻郡与那原町板良敷847−3' }
+      ]
+    }
+  }
+}
+</script>
