620ffb4c0315c5fe5df6c826eaa402b31aa89ec6
teppei-kaneshiro <teppei@miraistudio.co.jp>
2023-12-10 10:29:09 +0900

    add: 一覧画面draft

--- pages/spot_list.vue
+++ pages/spot_list.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-for="spot in spotList" :key="spot.id">
-      <mock-spot :spot-data="spot" />
+      <mock-spot :spot-data="spot" :icon="spot.icon" />
     </div>
   </div>
 </template>
@@ -10,10 +10,10 @@ export default {
   data () {
     return {
       spotList: [
-        { id: 1, congestion: '混雑', name: '美ら海水族館', address: '国頭郡本部町石川424' },
-        { id: 2, congestion: '普通', name: 'エイサー会場', address: '諸見里2丁目1-1' },
-        { id: 3, congestion: '空き', name: '琉球ガラス村', address: '糸満市福地169' },
-        { id: 4, congestion: '普通', name: '陶芸JIN', address: '島尻郡与那原町板良敷847−3' }
+        { id: 1, congestion: '混雑', name: '美ら海水族館', address: '国頭郡本部町石川424', icon: require('@/assets/upper_body-2.svg') },
+        { id: 2, congestion: '普通', name: 'エイサー会場', address: '諸見里2丁目1-1', icon: require('@/assets/upper_body-2.svg') },
+        { id: 3, congestion: '空き', name: '琉球ガラス村', address: '糸満市福地169', icon: require('@/assets/upper_body-2.svg') },
+        { id: 4, congestion: '普通', name: '陶芸JIN', address: '島尻郡与那原町板良敷847−3', icon: require('@/assets/upper_body-2.svg') }
       ]
     }
   }
