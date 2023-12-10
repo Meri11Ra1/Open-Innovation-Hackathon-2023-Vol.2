436eaa6a737d7539ca38354eb30e63efac4f9806
Meri11Ra1 <R.t.00293794@gmail.com>
2023-12-10 10:30:35 +0900

    change: 交換品をコンポーネント化したところ

--- pages/item_list.vue
+++ pages/item_list.vue
@@ -3,117 +3,40 @@
     <h1>
       交換品一覧
     </h1>
-    <div class="item_list">
-      <dl>
-        <input id="clothes_toggle" type="checkbox">
-        <label for="clothes_toggle">
-          <dt>
-            服
-          </dt>
-        </label>
-        <dd>
-          <ul>
-            <li>
-              <img src="../static/fashion_tshirt1_white.png" alt="yuhayuhaのプリントTシャツ">
-              <p>yuhayuhaのプリントTシャツ 50ポイント</p>
-              <button>交換する</button>
-            </li>
-          </ul>
-        </dd>
-      </dl>
-    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      itemList: [
+        { id: 1, congestion: '混雑', name: '美ら海水族館', address: '国頭郡本部町石川424' },
+        { id: 2, congestion: '普通', name: 'エイサー会場', address: '諸見里2丁目1-1' },
+        { id: 3, congestion: '空き', name: '琉球ガラス村', address: '糸満市福地169' },
+        { id: 4, congestion: '普通', name: '陶芸JIN', address: '島尻郡与那原町板良敷847−3' }
+      ]
+    }
+  }
+}
+</script>
 
 <style>
 h1 {
   text-align: center;
 }
 
-img {
-  width: 80%;
-}
-
-.item_list dl,
-.item_list dt,
-.item_list dd {
-margin: 0;
-padding: 0;
-}
-
-.item_list dl+dl {
-margin-top: 1rem;
-}
-
-.item_list dt,
-.item_list dd {
-padding: 10px;
-}
-
-.item_list dl {
-position: relative;
-overflow: hidden;
-}
-
-.item_list dl>input {
-display: none;
-}
-
-.item_list dt {
-position: relative;
-z-index: 1;
-padding-right: 40px;
-cursor: pointer;
-background: #e6e6e6;
-transition: .4;
-border: solid 1px #adadad;
-}
-
-.item_list dd {
-position: absolute;
-visibility: hidden;
-transform: translateY(-100%);
-transition: .4s;
-background: #f5f5f5;
-border: solid 1px #adadad;
-border-top: none;
-}
-
-.item_list dl>input:checked+label+dd {
-position: relative;
-visibility: visible;
-transform: translateY(0);
-}
-
-.item_list dt::before {
-content: ”;
-position: absolute;
-top: 50%;
-right: 10px;
-border-style: solid;
-border-width: 17.3px 10px 0 10px;
-border-color: #666 transparent transparent transparent;
-transform: translateY(-50%);
-transition: .4s;
-}
-
-.item_list dl>input:checked+label>dt::before {
-transform: translateY(-50%) rotate(180deg);
-}
-
-ul {
-  all: unset;
-}
-
-li {
-  all: unset;
-  display: flex;
-  flex-flow: column;
-  justify-content: center;
-  align-items: center;
-}
 p, button , label{
   font-size: 4.5vw;
+}
+
+button {
+  width: 50%;
+  border: none;
+  border-radius: 3vh;
+  background-color: rgb(54, 155, 255);
+  color: white;
+  padding: 1.5vh;
 }
 
 </style>
